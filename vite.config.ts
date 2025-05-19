e6d2df747acf14d1b71f515429eb3144fd4e3806
DavidHernan3 <fp_hernandezdavid@ciudadjardin.org>
2025-05-19 14:11:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // ✅ ¡Esta línea es la clave para Azure!
+
   server: {
     host: "::",
     port: 8080,
